d6ee728ee58831ac53effb77252c99936f26d5da
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-07 10:58:05 -0700

    Adds code for user_story_3, passing.

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -2,4 +2,9 @@ class Mission < ApplicationRecord
   validates_presence_of :title, :time_in_space
   has_many :astronaut_missions
   has_many :astronauts, through: :astronaut_missions
+
+  def self.alphabetical_missions
+    @missions = Mission.all
+    @missions.order('title ASC')
+  end
 end
