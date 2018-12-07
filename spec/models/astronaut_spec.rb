739acc76a5d1b139985281ee9ee55057ed8380ce
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-07 10:29:38 -0700

    Adds tests for user_story_2

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,16 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+
+  describe 'class methods' do
+    describe '.average_age' do
+      it 'returns mean age of all astronauts' do
+        astronaut_1 = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
+        astronaut_2 = Astronaut.create(name: "Barbara Streisand", age: 45, job: "Good Company")
+        astronaut_3 = Astronaut.create(name: "Cedric the Entertainer", age: 35, job: "Entertainer")
+
+        expect(Astronaut.average_age).to eq(39)
+      end
+    end
+  end
 end
